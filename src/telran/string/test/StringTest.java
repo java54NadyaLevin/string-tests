2db19e4ba201b96faff2a1f652f6710ca783db07
Nadelele <nadele@gmail.com>
2024-03-05 07:59:27 +0200

    initial commit - to remember the process of using git in eclipse

---
+++ src/telran/string/test/StringTest.java
@@ -0,0 +1,25 @@
+package telran.string.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import static java.lang.String.*;
+import org.junit.jupiter.api.Test;
+
+
+class StringTest {
+
+	@Test
+	@DisplayName("test of the method concat")
+	void concatTest() {
+		String hello = "Hello";
+		assertEquals("Hello World!!", hello.concat(" World!").concat("!"));
+				
+	}
+	
+	void containsTest() {
+		String hello = "Hello";
+		assertTrue(hello.contains("ell"));
+		assertFalse(hello.contains("lll"));
+	}
+
+}
